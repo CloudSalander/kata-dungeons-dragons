e48c7cf781d146b9a5388048a3901486568afdc9
Cloud <ruben.as532@gmail.com>
2024-04-25 10:41:22 +0200

    Program Main Structure

---
+++ class/Mage.php
@@ -0,0 +1,4 @@
+<?php 
+class Mage extends Player {}
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once('class/Player.php');
+include_once('class/Warrior.php');
+include_once('class/Archer.php');
+include_once('class/Mage.php');
+
+$warrior = new Warrior();
+$mage = new Mage();
+$archer = new Archer();
+
+
+
+
+?>
